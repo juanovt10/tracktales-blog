05e004f2e59224c6e7b38e13f85d312bcc3f5a71
juanovt10 <>
2023-11-26 15:26:07 +0000

    amend post and username models

--- postBoard/models.py
+++ postBoard/models.py
@@ -3,7 +3,17 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+WORLD_AREAS = [
+    ("north_america", "North America"),
+    ("south_america", "South America"),
+    ("europe", "Europe"),
+    ("south_asia", "South Asia"), 
+    ("east_asia", "East Asia"),
+    ("west_asia", "West Asia"),
+    ("oceania", "Oceania"),
+    ("africa", "Africa"),
+    ("middle_east", "Middle East"),
+]
 
 class Tag(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -15,7 +25,8 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    type_tags = models.ManyToManyField(Tag) 
+    type_tags = models.ManyToManyField(Tag)
+    world_area = models.CharField(max_length=50, choices=WORLD_AREAS, default='europe')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="board_posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -46,27 +57,14 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
-
-    
-WOLRD_AREAS = [
-    ("north_america", "North America"),
-    ("south_america", "South America"),
-    ("europe", "Europe"),
-    ("south_asia", "South Asia"), 
-    ("east_asia", "East Asia"),
-    ("weat_asia", "West Asia"),
-    ("oceania", "Oceania"),
-    ("africa", "Africa"),
-    ("middle_east", "Middle East"),
-]
+        return f"Comment {self.body} by {self.author.username}"
 
 
 class UserProfile(models.Model):
     username = models.CharField(max_length=255, unique=True)
     display_name = models.TextField()
     user_description = models.TextField()
-    most_visited_area = models.CharField(max_length=50, choices=WOLRD_AREAS)
+    most_visited_area = models.CharField(max_length=50, choices=WORLD_AREAS)
     languages = models.TextField(blank=True, null=True)
     countries_traveled = models.IntegerField(default=1)
     sign_on = models.BooleanField(default=False)
--- postBoard/views.py
+++ postBoard/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 def index(request):
     return render(request, 'index.html')
+
+class PostBoard(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(approved=True).order_by('-created_on')
+    template_name = 'board.html'
