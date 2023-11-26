0ad64a8162349d3d11c8fe06c4ddbbc36786643f
juanovt10 <>
2023-11-26 17:47:25 +0000

    Initial board template design

--- postBoard/urls.py
+++ postBoard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, PostBoard
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('board', PostBoard.as_view(), name='post_board')
 ]
