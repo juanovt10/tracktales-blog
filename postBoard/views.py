cf4d0485dba5425591e3c2e97a94e4d83dedd448
juanovt10 <>
2023-11-29 20:41:54 +0000

    Started developing liked counter functionality in views.py and the board template

--- postBoard/views.py
+++ postBoard/views.py
@@ -16,6 +16,54 @@ class PostBoard(generic.ListView):
     template_name = 'board.html'
     context_object_name = 'posts'
 
+    # def get(self, request, slug=None, *args, **kwargs):
+    #     if slug is not None:
+    #         queryset = Post.objects
+    #         post = get_object_or_404(queryset, slug=slug)
+    #         comments = post.comments.filter(approved=True).order_by('created_on')
+    #         like = False
+    #         if request.user.is_authenticated and post.likes.filter(id=request.user.id).exists():
+    #             liked = True
+
+    #         return render(
+    #             request,
+    #             'board.html',  
+    #             {
+    #                 'post': post,
+    #                 'comments': comments,
+    #                 'liked': liked
+    #             },
+    #         )
+    #     else:
+            
+    #         posts = Post.objects.all()
+    #         return render(
+    #             request,
+    #             'board.html',  
+    #             {
+    #                 'posts': posts,
+    #             },
+    #         )
+
+    # def get(self, request, slug, *args, **kwargs):
+    #     queryset: Post.objects
+    #     post = get_object_or_404(queryset, slug=slug)
+    #     comments = post.comments.filter(approved=True).order_by('created_on')
+    #     liked = False
+    #     if post.likes.filter(id=self.request.user.id).exists():
+    #         liked = True
+
+    #     return render(
+    #         request,
+    #         'board.html',
+    #         {
+    #             'post': post,
+    #             "comments": comments,
+    #             "liked": liked
+    #         },
+    #     )
+
+
     # method to display world_areas and tags lists in post form
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -24,6 +72,8 @@ class PostBoard(generic.ListView):
         context['post_form'] = PostForm()
         return context
 
+
+    #method to post the user post into the database
     def post(self, request, *args, **kwargs):
         post_form = PostForm(data=request.POST)
         
@@ -33,4 +83,17 @@ class PostBoard(generic.ListView):
             post_form.save()
         else:
             post_form = PostForm()
+        return HttpResponseRedirect(reverse_lazy('post_board'))
+
+
+class PostLike(View):
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
         return HttpResponseRedirect(reverse_lazy('post_board'))
