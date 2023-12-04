fd2f78aba7c2eeea6185b8dc7e27c5e3c132246d
juanovt10 <>
2023-12-04 10:24:31 +0000

    Add toggling feature for commenting area

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,7 @@
-// document.addEventListener("DOMContentLoaded", function () {
-
-//     let buttons = document.getElementsByTagName('button');
-//     let commentArea = document.getElementById("user-comment-area");
-
-//     let editProfileBtn = document.getElementById("edit-profile-btn");
-//     let editProfilePopup = document.getElementById("edit-profile-popup");
-
-//     editProfileBtn.addEventListener("click", function() {
-//         editProfilePopup.classList.add()
-        
-//     });
-
-    // for (let button of buttons) {
-    //     button.addEventListener('click', function () {
-    //         if (this.getAttribute('data-type') === 'display-comment') {
-    //             commentArea.classList.remove("comment-hide");
-    //         } else if (this.getAttribute('data-type') === 'edit-profile') {
-
-    //         }
-    //     });
-    // }
-// });
-
-// $('#deletePostModal').on('show.bs.modal', function (event) {
-//     var button = $(event.relatedTarget);
-//     var postId = button.data('post-id');
-//     var postTitle = button.data('post-title');
-
-//     var modal = $(this);
-//     modal.find('#postTitlePlaceholder').text(postTitle);
-//     modal.find('form').attr('action', '{% url "delete_post" post_id=0 %}'.replace('0', postId));
-// });
+$(document).ready(function () {
+    // This allow the toggling of the comment area to be displayed when is clicked
+    $(".display-comment-btn").on("click", function () {
+        let postId = $(this).data("post-id");
+        $(".user-comment-area[data-post-id='" + postId + "']").toggle();
+    });
+});
