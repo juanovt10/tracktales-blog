d58ed837ab03103cf8deeeb2d69f67eff65a8b3f
juanovt10 <>
2023-12-08 13:05:25 +0000

    Create contact us data model

--- postBoard/models.py
+++ postBoard/models.py
@@ -99,3 +99,15 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+
+class ContactInfo(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.subject}" 
