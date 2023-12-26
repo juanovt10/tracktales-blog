cb57d3405ba44d10db8085bf5570e3b1a295d425
Juan Velasquez <juanovelasquez10@gmail.com>
2023-12-26 15:34:28 +0000

    attempt to solve deployment issue

--- tracktales/settings.py
+++ tracktales/settings.py
@@ -144,7 +144,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 
